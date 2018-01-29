0de5dc024c2b6b453e17ec6fa5fc76034afa982b
Emil Folino BTH MAC <emilfolino@gmail.com>
2018-01-29 15:31:48 +0100

    Did enough

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
 const teachers = require("./teachers.js");
 
-teachers.test();
+
+/**
+ * Main function.
+ * @returns void
+ */
+(function() {
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.setPrompt("skolan: ");
+    rl.prompt();
+
+    rl.on("close", exitProgram);
+    rl.on("line", (line) => {
+        line = line.trim();
+        switch (line) {
+            case "quit":
+            case "exit":
+                exitProgram();
+                break;
+            case "help":
+            case "menu":
+                showMenu();
+                rl.prompt();
+                break;
+            case "larare":
+                teachers.larare(rl);
+                break;
+            case "larare":
+                teachers.larare(rl);
+                break;
+            default:
+                console.log("No option called that");
+                showMenu();
+                rl.prompt();
+                break;
+        }
+
+    });
+})();
+
+
+
+/**
+ * Close down program and exit with a status code.
+ *
+ * @param {number} code Exit with this value, defaults to 0.
+ *
+ * @returns {void}
+ */
+function exitProgram(code) {
+    code = code || 0;
+
+    console.info("Exiting");
+    process.exit(code);
+}
+
+
+
+/**
+ * Show the menu on that can be done.
+ *
+ * @returns {void}
+ */
+function showMenu() {
+    console.info(
+        ` You can choose from the following commands.\n`
+        + `  exit, quit, ctrl-d - to exit the program.\n`
+        + `  help, menu - to show this menu.\n`
+        + `  cheat      - show the current number.\n`
+        + `  init       - randomize a new number.\n`
+        + `  anything else is treated as a guess.`
+    );
+}
--- teachers.js
+++ teachers.js
@@ -9,18 +9,43 @@ const teachers = {
         const db     = await mysql.createConnection(config);
         return db;
     },
-    test: async function () {
+    larare: async function (rl) {
         const db = await teachers.connect();
 
         let sql;
         let res;
 
-        sql = "SELECT 1+1 AS Sum";
+        sql = "SELECT *, TIMESTAMPDIFF(YEAR, fodd, CURRENT_TIMESTAMP()) as alder FROM larare";
         res = await db.query(sql);
 
-        console.info(res);
+        console.log(teachers.teacherAsTable(res));
 
         db.end();
+
+        rl.prompt();
+    },
+    teacherAsTable(res) {
+        let str;
+
+        str  = "+-----------+---------------------+-----------+----------+----------+\n";
+        str += "| Akronym   | Namn                | Avdelning | Ålder    |   Lön    |\n";
+        str += "|-----------|---------------------|-----------|----------|----------|\n";
+        for (const row of res) {
+            str += "| ";
+            str += row.akronym.padEnd(10);
+            str += "| ";
+            str += (row.fornamn + " " + row.efternamn).padEnd(20);
+            str += "| ";
+            str += row.avdelning.padEnd(10);
+            str += "| ";
+            str += row.alder.toString().padStart(8);
+            str += "| ";
+            str += row.lon.toString().padStart(8);
+            str += " |\n";
+        }
+        str += "+-----------+---------------------+-----------+----------+----------+\n";
+
+        return str;
     }
 };
 
